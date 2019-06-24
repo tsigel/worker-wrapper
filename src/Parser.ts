ae55cf98607a1cf4a9f6b7c5046dd4e53bc8c0b1
tsigel <tsddaniil@gmail.com>
2019-06-24 13:39:07 +0300

    Refactor parser

--- src/Parser.ts
+++ src/Parser.ts
@@ -7,13 +7,15 @@ export class Parser {
     private readonly _classes: Record<string, any> = Object.create(null);
     private _cache: Array<{ code: string; compiled: any }> = [];
     private static _canUseUrl: boolean = true;
+    private static _compileCount: number = 0;
+
 
     constructor() {
         this._classes = Object.create(null);
     }
 
     public parse(data: ISerialized): any {
-        data.classes.forEach(this._addClassData, this);
+        // data.classes.forEach(this._addClassData, this);
 
         const loop = (item: any): any => {
             if (Parser._isSerializedField(data.data)) {
@@ -46,11 +48,11 @@ export class Parser {
         // TODO need refactor
         switch (data.__type) {
             case 'serialized-class':
-                return this._parseFunc(classes[data.index].template);
+                return Parser._compile(classes[data.index].template);
             case 'serialized-function':
-                return this._parseFunc(data.template);
+                return Parser._compile(data.template);
             case 'serialized-instance':
-                return this._parseInstance(data.data, this._parseFunc(classes[data.index].template));
+                return this._parseInstance(data.data, Parser._compile(classes[data.index].template));
         }
     }
 
@@ -93,18 +95,59 @@ export class Parser {
         ].indexOf(data.__type) !== -1;
     }
 
-    private _compile(data: { name: string, code: string }): Promise<any> {
+    private static _compile(code: string): Promise<any> {
         if (Parser._canUseUrl) {
-            return Parser.evalByUrl(data)
-                .catch(e => {
-                    // TODO add check url error
+            return Parser._evalByUrl(code)
+                .catch(() => {
                     Parser._canUseUrl = false;
+                    return Parser._eval(code);
                 });
         }
+        return Promise.resolve(Parser._eval(code));
     }
 
-    private static evalByUrl(code: string): Promise<any> {
+    private static _eval(code: string): any {
+        const name = Parser._getCompileName() as keyof typeof self;
+        const template = `self['${name}'] = ${code}`;
+        eval(template);
+        return self[name];
+    }
 
+    private static _evalByUrl(code: string): Promise<any> {
+        const name = Parser._getCompileName() as keyof typeof self;
+        const template = `self['${name}'] = ${code}`;
+        const url = URL.createObjectURL(new Blob([template], { type: 'application/json' }));
+
+        return Parser._addScript(url)
+            .then(() => {
+                return self[name];
+            });
+    }
+
+    private static _getCompileName() {
+        return `compile_${Parser._compileCount++}`;
+    }
+
+    private static _loadScript(url: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const script = document.createElement('script');
+            script.addEventListener('load', () => resolve());
+            script.addEventListener('error', reject);
+            script.src = url;
+            document.body.appendChild(script);
+        });
+    }
+
+    private static _addScript(url: string): Promise<void> {
+        if ('importScripts' in self) {
+            try {
+                (self as WorkerUtils).importScripts(url);
+                return Promise.resolve();
+            } catch (e) {
+                return Promise.reject(e);
+            }
+        }
+        return Parser._loadScript(url);
     }
 
 }
